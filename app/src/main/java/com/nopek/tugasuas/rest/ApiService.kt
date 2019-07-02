c1126cb6ed8cfa1e7b48e59275a18befd8c9218b
aditiotri <yayukwahyuningsih98@gmail.com>
2019-07-02 13:27:32 +0700

    first commit

---
+++ app/src/main/java/com/nopek/tugasuas/rest/ApiService.kt
@@ -0,0 +1,18 @@
+package com.nopek.tugasuas.rest
+
+import com.nopek.tugasuas.BuildConfig
+import com.nopek.tugasuas.model.RootDataModel
+import retrofit.http.GET
+import rx.Observable
+
+interface ApiService {
+    // http://informatika.upgris.ac.id/mobile/data/index.php/api/example/detil_jadwal?key=33c227f799464dac08f60f1b0d5770&npm=16670066
+    @GET("detil_jadwal?key=" + BuildConfig.API_KEY + "&npm=" + BuildConfig.NPM)
+    fun getJadwalKuliah(
+    ): Observable<RootDataModel>
+
+    @GET("detil_pribadi?key=" + BuildConfig.API_KEY + "&npm=" + BuildConfig.NPM)
+    fun getProfil(
+    ): Observable<RootDataModel>
+
+}
